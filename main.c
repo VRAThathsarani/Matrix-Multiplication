0bf640598eccd244ac818c1e0e69b460d14cc9b3
VRAThathsarani <148706621+VRAThathsarani@users.noreply.github.com>
2023-12-15 08:22:57 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -5,20 +5,20 @@ int main ()
 {
     int m,n;
     int i,j;
-    int product[m][1];
     int sum;
 
 
     printf("Enter the rows of the Table A:");
     scanf("%d",&m);
-    printf("Enter the columns of the Table A and Enter the rows of the matrix X:");
+    printf("Enter the columns of the Table A (the rows of the matrix X):");
     scanf("%d",&n);
 
     int A[m][n];
     int X[n][1];
     int Y[m][1];
+    int product[m][1];
 
-    printf("Enter the elements of Table A:\n");
+    printf("\nEnter the elements of Table A:\n");
 
     for (i=0; i<m; i++)
     {
@@ -28,14 +28,32 @@ int main ()
         }
     }
 
-    printf("Enter the elements of Table X:\n");
+    printf("\nEnter the elements of Table X:\n");
 
     for (j=0; j<n; j++)
     {
         scanf("%d",&X[j][0]);
     }
 
-    printf("AX=Y\n");
+    printf("\nTable A:\n");
+
+    for (i=0; i<m; i++)
+    {
+        for (j=0; j<n; j++)
+            {
+                printf("|%d|",A[i][0]);
+            }
+            printf("\n");
+    }
+
+    printf("\nTable X:\n");
+
+    for (i=0; i<n; i++)
+    {
+        printf("|%d|\n",X[i][0]);
+    }
+
+    printf("\n");
 
     for (i=0; i<m; i++)
     {
@@ -47,11 +65,11 @@ int main ()
         sum=0;
     }
 
-    printf("Resulting Matrix\n");
+    printf("Resulting Table\n");
 
     for (i=0; i<m; i++)
     {
-        printf("%d\n",Y[i][0]);
+        printf("|%d|\n",Y[i][0]);
     }
 
 
